dbd27d87bc98d948bf8447f2c239d32826a7f3d0
Junior <jhimmayati17@gmail.com>
2016-11-05 23:53:58 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget2/MainActivity.java
@@ -0,0 +1,25 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindColor;
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.editTextNama)
+    EditText etNama;
+    @BindView(R.id.textViewHasil)
+    TextView tvHasil;
+    @BindColor(R.color.colorAccent)
+    int colorAccent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
